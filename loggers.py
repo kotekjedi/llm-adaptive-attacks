78771420270e525c48d5bcf4b28c408a0a05efb7
kotekjedi <sasha_pusha@mail.de>
2024-05-27 23:13:11 +0200

    new wandb test class

--- loggers.py
+++ loggers.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from datetime import datetime
 
 import pandas as pd
@@ -6,19 +7,42 @@ import pytz
 
 import wandb
 
+# class WandBLogger:
+#     """WandB logger."""
+
+#     def __init__(self, args, project_name="llm-rs"):
+#         self.logger = wandb.init(
+#             project = project_name,
+#             config = vars(args),
+#         )
+
+#     def log(self, dict):
+#         self.logger.log(dict)
+
+#     def finish(self):
+#         self.logger.finish()
 
 class WandBLogger:
     """WandB logger."""
 
     def __init__(self, args, project_name="llm-rs"):
+        # Set W&B to offline mode
+        os.environ['WANDB_MODE'] = 'offline'
+
+        # Initialize W&B run
         self.logger = wandb.init(
-            project = project_name,
-            config = vars(args),
+            project=project_name,
+            config=vars(args),
         )
+        self.run_id = self.logger.id  # Store the run ID
 
     def log(self, dict):
         self.logger.log(dict)
 
     def finish(self):
         self.logger.finish()
+        # Sync the specific run after training
+        run_directory = os.path.join("wandb", "offline-run-logs", self.run_id)
+        subprocess.run(["wandb", "sync", run_directory])
+
 
